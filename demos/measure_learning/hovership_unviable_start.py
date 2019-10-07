40917e7ab76aef03148a24f79d5b8cfce44b1349
Alex <nalynkal@gmx.de>
2019-10-07 02:16:39 +0200

    Redone examples with missing data and added new plot script to plot recorded data again. Part 2

--- demos/measure_learning/hovership_unviable_start.py
+++ demos/measure_learning/hovership_unviable_start.py
@@ -21,8 +21,9 @@ def run_demo(dynamics_model_path = './data/dynamics/', gp_model_path = './data/g
 
     # A prior state action pair that is considered safe (from system knowledge)
     # Here it is chosen to be outside the viable set to demonstrate that the learner can deal with this case
-    X_seed = np.atleast_2d(np.array([1.45, .5]))
-    y_seed = np.array([[.5]])
+    # X_seed = np.atleast_2d(np.array([1.45, .5]))
+    X_seed = np.atleast_2d(np.array([1.6, .5]))
+    y_seed = np.array([[.8]])
 
     seed_data = {'X': X_seed, 'y': y_seed}
 
@@ -31,9 +32,9 @@ def run_demo(dynamics_model_path = './data/dynamics/', gp_model_path = './data/g
                             prior_model_path=gp_model_file,
                             learn_hyperparameters=False)
 
-    sampler.exploration_confidence_s = 0.98
+    sampler.exploration_confidence_s = 0.95
     sampler.exploration_confidence_e = 0.9999
-    sampler.measure_confidence_s = 0.75
+    sampler.measure_confidence_s = 0.80
     sampler.measure_confidence_e = 0.9999
     sampler.safety_threshold_s = 0.0
     sampler.safety_threshold_e = 0.15
